8ae8c197d806c65260683df415e8ceff8ea6a1ce
Srijan Srivastava <srijan.srivastav2@gmail.com>
2021-04-29 18:08:09 +0530

    Extension fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import { Component } from 'react';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state ={
-      string: 'Hello Srijan'
-    };
-  }
-  render() {
-    return(
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={() => this.state.string.includes('Srijan') ? this.setState({ string: "Hello Mansi!" }) : this.setState({ string: "Hello Srijan!" })}>Change Text</button>
-      </header>
-    </div>
-    );
-  }
+		this.state = {
+			string: 'Hello Srijan',
+		};
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+
+					<p>{this.state.string}</p>
+
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+
+					<button
+						onClick={() =>
+							this.state.string.includes('Srijan')
+								? this.setState({ string: 'Hello Mansi!' })
+								: this.setState({ string: 'Hello Srijan!' })
+						}
+					>
+						Change Text
+					</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
