09b9837404d18d0efe374bd6444bfa0d9dd5f69e
Srijan Srivastava <srijan.srivastav2@gmail.com>
2021-04-29 05:37:10 +0530

    Changing to class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state ={
+      string: 'Hello Srijan'
+    };
+  }
+  render() {
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {this.state.string}
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => this.state.string.includes('Srijan') ? this.setState({ string: "Hello Mansi!" }) : this.setState({ string: "Hello Srijan!" })}>Change Text</button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
