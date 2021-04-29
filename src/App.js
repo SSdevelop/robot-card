87d3314b2d6094e47121e32bf7763f0d13a4848b
Srijan Srivastava <srijan.srivastav2@gmail.com>
2021-04-30 00:43:54 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import { Component } from 'react';
 import './App.css';
-import logo from './logo.svg';
+import { CardList } from './component/card-list/card-list.component';
+import { SearchBox } from './component/search-box/search-box.component';
 
 class App extends Component {
 	constructor() {
 		super();
 
 		this.state = {
-			string: 'Hello Srijan',
+			monsters: [],
+			searchField: '',
 		};
+
+		// this.handleChange = this.handleChange.bind(this);
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((user) => this.setState({ monsters: user }))
+			.then((st) => console.log(this.state))
+			.catch((err) => console.log('Error found!'));
+	}
+
+	handleChange(e) {
+		this.setState({ searchField: e.target.value });
 	}
 
 	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
 		return (
 			<div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-
-					<p>{this.state.string}</p>
-
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-
-					<button
-						onClick={() =>
-							this.state.string.includes('Srijan')
-								? this.setState({ string: 'Hello Mansi!' })
-								: this.setState({ string: 'Hello Srijan!' })
-						}
-					>
-						Change Text
-					</button>
-				</header>
+				<h1 className='heading'>Monster Info!!</h1>
+				<SearchBox
+					placeholder='Search monsters'
+					handleChange={this.handleChange.bind(this)}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
